0b318517bef5cbbba151f8fb0f9dcadca7ddc730
Testing Waschag <waschag@tvk.rwth-aachen.de>
2017-07-30 16:12:07 +0200

    add some charts to stats page

--- wasch/urls.py
+++ wasch/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
@@ -11,4 +12,12 @@ urlpatterns = [
     # url(r'^login/check/$', views.check_login_view, name='check_login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/wasch/'), name='logout'),
     url(r'^stats/$', views.stats, name='stats'),
+    url(
+        r'^statsapi_appointments_per_day/$',
+        login_required(views.AppointmentsPerDayChart.as_view()),
+        name='statsapi_appointments_per_day'),
+    url(
+        r'^statsapi_appointments_per_floor/$',
+        login_required(views.AppointmentsPerFloorChart.as_view()),
+        name='statsapi_appointments_per_floor'),
 ]
--- wasch/views.py
+++ wasch/views.py
@@ -1,7 +1,10 @@
+import datetime
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout as auth_logout
+from chartjs.views.lines import BaseLineChartView
 from legacymodels import Users, Termine, Waschmaschinen
 
 
@@ -27,12 +30,83 @@ def check_login_view(request):
 @login_required
 def stats(request):
     """Show usage stats"""
-    users = Users.select().execute()
-    machines = Waschmaschinen.select().execute()
-    appointents = Termine.select().execute()
+    users = Users.select(Users.login).execute()
+    machines = Waschmaschinen.select(Waschmaschinen.id).execute()
+    appointents = Termine.select(Termine.user).execute()
     context = {
         'userscount': len(users),
         'machinescount': len(machines),
         'appointmentscount': len(appointents),
     }
     return render(request, 'wasch/stats.html', context)
+
+
+def _appointments_per_day(day, used=None):
+    query = Termine.select(Termine.datum, Termine.wochentag).where(
+        Termine.datum.year == day.year,
+        Termine.datum.month == day.month,
+        Termine.datum.day == day.day,
+    )
+    if used is not None:
+        query = query.where(
+            (Termine.wochentag == 8) == used,
+        )
+    return len(query.execute())
+
+
+def _appointments_per_floor(floor, used=None):
+    users = Users.select(Users.id).where(
+        Users.zimmer > 100 * floor,
+        Users.zimmer < 100 * (floor + 1),
+    )
+    count = 0
+    for user in users:
+        query = Termine.select(Termine.wochentag).where(Termine.user == user)
+        if used is not None:
+            query = query.where(
+                (Termine.wochentag == 8) == used,
+            )
+        count += len(query.execute())
+    return count
+
+
+class AppointmentsPerDayChart(BaseLineChartView):
+    def __init__(self, *args, **kwargs):
+        BaseLineChartView.__init__(self, *args, **kwargs)
+        end = datetime.date.today()
+        self.days = [
+            end + datetime.timedelta(days=d) for d in range(-30, 0, 3)]
+
+    def get_labels(self):
+        """Return 10 labels for the x-axis."""
+        return [str(d) for d in self.days]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Appointments", "Used"]
+
+    def get_data(self):
+        """Return 2 datasets to plot."""
+        return [
+            [_appointments_per_day(day) for day in self.days],
+            [_appointments_per_day(day, used=True) for day in self.days],
+        ]
+
+
+class AppointmentsPerFloorChart(BaseLineChartView):
+    floors = list(range(0, 17))
+
+    def get_labels(self):
+        """Return 10 labels for the x-axis."""
+        return [str(f) for f in self.floors]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Appointments", "Used"]
+
+    def get_data(self):
+        """Return 2 datasets to plot."""
+        return [
+            [_appointments_per_floor(floor) for floor in self.floors],
+            [_appointments_per_floor(floor, used=True) for floor in self.floors],
+        ]
