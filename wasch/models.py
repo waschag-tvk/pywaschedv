cd887038c1ee7f79c44ca9e70b07ee58a966c8ff
Keegan Brown <keeganbrown@hotmail.com>
2017-07-31 18:13:07 +0200

    Added initial basic models

--- wasch/models.py
+++ wasch/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.TextField()
+    isActivated = models.BooleanField()
+    status = models.SmallIntegerField()
+
+    class Meta:
+        db_table = 'users'
+
+
+class WashingMachines(models.Model):
+    number = models.SmallIntegerField(unique=True)
+    isAvailable = models.BooleanField()
+    notes = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = 'washingmachines'
+
+
+class Appointments(models.Model):
+    time = models.DateTimeField()
+    user = models.ForeignKey(Users)
+    machine = models.ForeignKey(WashingMachines)
+    isBonus = models.BooleanField()
+    wasUsed = models.BooleanField()
+
+    class Meta:
+        db_table = 'appointments'
+
+
+class WashParameters(models.Model):
+    name = models.CharField(max_length=20)
+    value = models.CharField(max_length=20)  # convert this to correct format, depending on parameter
+
+    class Meta:
+        db_table = 'washparameters'
