f52db4c0cb8eaa46b47e2b965fa396100b37a69c
Yushin Washio <yuwash@yandex.com>
2017-08-01 21:36:47 +0200

    add admin area for WaschUser

--- wasch/admin.py
+++ wasch/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from wasch.models import WashUser
 
-# Register your models here.
+
+class WashUserAdmin(admin.ModelAdmin):
+    list_display = ['user', 'isActivated', 'status']
+    ordering = ['user']
+
+
+admin.site.register(WashUser, WashUserAdmin)
