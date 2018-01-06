521187ef9afb8194f5a7f98d605ecd548cf7e0e2
Yushin Washio <yuwash@yandex.com>
2018-01-06 14:09:53 +0100

    add book view to urls, menu

--- wasch/urls.py
+++ wasch/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='wasch/login.html'), name='login'),
     # url(r'^login/check/$', views.check_login_view, name='check_login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/wasch/'), name='logout'),
+    url(r'^book/$', views.book, name='book'),
     url(r'^stats/$', views.stats, name='stats'),
     url(r'^status/$', views.status, name='status'),
     url(
