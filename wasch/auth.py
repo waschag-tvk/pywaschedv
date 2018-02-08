f1266c03375373cea222335bf58e8046de039b79
Yushin Washio <yuwash@yandex.com>
2018-02-08 19:07:43 +0400

    auth GodOnlyBackend move out god creation into method get_or_create_god

--- wasch/auth.py
+++ wasch/auth.py
@@ -1,20 +1,32 @@
 import crypt
 from django.contrib.auth.models import User
+from wasch.models import WashUser
+
 
 class GodOnlyBackend:
-    god = User(username='WaschRoss')
+    god_username = 'WaschRoss'
+
+    @classmethod
+    def get_or_create_god(cls, create_washgod=True):
+        try:
+            god = User.objects.get(username=cls.god_username)
+        except User.DoesNotExist:
+            god = User(
+                username=cls.god_username,
+                is_staff=True,
+                is_superuser=True,
+            )
+            god.save()
+        if create_washgod and not WashUser.objects.filter(user=god).exists():
+            washgod = WashUser(
+                user=GodOnlyBackend.god, isActivated=True, status=9)
+            washgod.save()
+        return god
 
     def authenticate(self, request, username=None, password=None):
-        if username == self.god.username \
+        if username == self.god_username \
                 and crypt.crypt(password, 'waschpulver') == 'waxudoXH6TLm.':
-
-            try:
-                return User.objects.get(username=self.god.username)
-            except User.DoesNotExist:
-                self.god.is_staff = True
-                self.god.is_superuser = True
-                self.god.save()
-            return self.god
+            return self.get_or_create_god()
         else:
             return None
 
