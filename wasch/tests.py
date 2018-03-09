b0027ee2fe9614dc9fd4977d61e25f8332c4907a
Yushin Washio <yuwash@yandex.com>
2018-03-09 10:34:27 +0100

    add more tests

--- wasch/tests.py
+++ wasch/tests.py
@@ -24,12 +24,21 @@ class WashUserTestCase(TestCase):
 
 class AppointmentTestCase(TestCase):
     exampleUserName = 'waschexample'
-    exampleTime = datetime.datetime(2015, 12, 12)
-    exampleMachine = tvkutils.get_or_create_machines()[0][0]
+    examplePoorUserName = 'poor'
+    exampleTime = Appointment.manager.scheduled_appointment_times()[-1]
+    exampleTooOldTime = datetime.datetime(1991, 12, 25)
+    exampleMachine, exampleBrokenMachine, lastMachine = \
+        tvkutils.get_or_create_machines()[0]
 
     def setUp(self):
         tvkutils.setup()
+        self.exampleMachine.isAvailable = True  # though this is default
+        self.exampleMachine.save()
+        self.exampleBrokenMachine.isAvailable = False
+        self.exampleMachine.save()
         WashUser.objects.create_enduser(self.exampleUserName, isActivated=True)
+        WashUser.objects.create_enduser(
+            self.examplePoorUserName, isActivated=False)
 
     def _createExample(self):
         user = User.objects.get(username=self.exampleUserName)
@@ -42,3 +51,36 @@ class AppointmentTestCase(TestCase):
         self.assertEqual(result.time, self.exampleTime)
         self.assertEqual(result.machine, self.exampleMachine)
         self.assertEqual(result.user.username, self.exampleUserName)
+        self.assertTrue(Appointment.manager.appointment_exists(
+            result.time, result.machine))
+        self.assertFalse(Appointment.manager.bookable(
+            result.time, result.machine, result.user))
+        self.assertEqual(
+            Appointment.manager.why_not_bookable(
+                result.time, result.machine, result.user),
+            41,  # Appointment taken
+        )
+        result.cancel()
+        self.assertTrue(Appointment.manager.bookable(
+            result.time, result.machine, result.user))
+
+    def test_bookable(self):
+        user = User.objects.get(username=self.exampleUserName)
+        poorUser = User.objects.get(username=self.examplePoorUserName)
+        self.assertEqual(
+            Appointment.manager.why_not_bookable(
+                self.exampleTime, self.exampleMachine, poorUser),
+            31,  # User not active
+        )
+        self.assertTrue(Appointment.manager.bookable(
+            self.exampleTime, self.exampleMachine, user))
+        self.assertEqual(
+            Appointment.manager.why_not_bookable(
+                self.exampleTooOldTime, self.exampleMachine, user),
+            11,  # Unsupported time
+        )
+        self.assertEqual(
+            Appointment.manager.why_not_bookable(
+                self.exampleTime, self.exampleBrokenMachine, user),
+            21,  # Machine out of service
+        )
