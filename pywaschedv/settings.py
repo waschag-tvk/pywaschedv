7df66f3b68c70e5c4b8d904e91a988c177e45cc5
Yushin Washio <yuwash@yandex.com>
2018-05-19 15:13:20 +0200

    add djangorestframework-jwt as dependency, entry in settings

--- pywaschedv/settings.py
+++ pywaschedv/settings.py
@@ -137,3 +137,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django REST framework JWT
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
