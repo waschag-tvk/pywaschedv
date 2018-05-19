1fd1d4320b240809af8efadae89485172874a316
Yushin Washio <yuwash@yandex.com>
2018-05-19 15:14:23 +0200

    add token-auth url in enteapi

--- enteapi/urls.py
+++ enteapi/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 router = routers.DefaultRouter()
@@ -7,4 +8,5 @@ router.register(r'appointment', views.AppointmentViewSet, base_name='appointment
 
 urlpatterns = [
     url(r'^v1/', include(router.urls)),
+    url(r'^token-auth/', obtain_jwt_token),
 ]
